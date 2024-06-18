9473a4e6c7224898a739fe372b2d34462b0f546e
SungminPark1997 <72503832+SungminPark1997@users.noreply.github.com>
2024-06-18 16:55:40 +0900

    initialize

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,36 @@
-import React from "react";
-
-import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import Home from "./home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+const Wrapper = styled.div`
+  width: 100%
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Wrapper>
+      <RouterProvider router={router} />
+    </Wrapper>
   );
 }
 
---
+++ client/src/home.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #4caf50;
+`;
+export default function Home() {
+  return <Wrapper>여긴 홈이야</Wrapper>;
+}
