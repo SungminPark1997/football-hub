c977385a29a8f8c95307f6df21d7ddd8ab1fa828
SungminPark1997 <72503832+SungminPark1997@users.noreply.github.com>
2024-08-22 17:05:11 +0900

    modify server.js : add post

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 const express = require("express");
-const path = require("path");
-const app = express();
+const cors = require("cors");
 
-app.listen(8080, function () {
-  console.log("listening on 8080");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from the server!" });
+});
+app.post("/api/post", (req, res) => {
+  const { title, content } = req.body;
+  console.log("Received data:", { title, content });
+
+  // 데이터 처리 후 응답
+  res.status(200).json({ message: "Post received successfully!" });
+});
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
